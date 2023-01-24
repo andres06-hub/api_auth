09f5f7bbc4d4cdd1d9343ba040ac4228120bcdec
andres06-hub <andressierrarojas7@gmail.com>
2023-01-24 02:24:13 -0500

    ADD: JWT and Strategy for get Token And Validate

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,20 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { userProviders } from './providers/user.providers';
 import { DatabaseModule } from 'src/providers/database/database.module';
+import { BcryptService } from 'src/common/services/bcrypt/bcrypt.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JWTKEYS } from 'src/common/constants/jwt.constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    JwtModule.register({
+      secret: JWTKEYS.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [...userProviders, AuthService],
+  providers: [...userProviders, AuthService, BcryptService, JwtStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,48 +1,60 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from 'src/models/user.entity';
-import { hash, compare } from 'bcrypt';
 import { DataSignUp } from './dto/signUp.dto';
+import { BcryptService } from 'src/common/services/bcrypt/bcrypt.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject('USER_REPOSITORY')
     private userRepository: Repository<User>,
+    private _bcryptSrv: BcryptService,
+    private _jwtSrv: JwtService,
   ) {}
   private logger = new Logger();
 
-  async login(email: string, password: string): Promise<User | null> {
+  // eslint-disable-next-line prettier/prettier
+  async login(email: string, password: string): Promise<string | boolean | null> {
     const findUser: User | null | false = await this.findOneByMail(email);
-    console.log('USER::', findUser);
-    if (!findUser) return null; // NOt found
+    if (findUser === null) return null; // NOt found
+    if (findUser === false) return false; //Error
+    this.logger.log('User Exists!');
     //Validate pass
-    const pwd: boolean = await this.unencrypt(findUser.password, password);
+    const pwd: boolean = await this._bcryptSrv.unencrypt(
+      findUser.password,
+      password,
+    );
+    if (!pwd) return 'incorrect';
     console.log(pwd);
     //Create JWT
+    const payload = {
+      id: findUser.id,
+      username: findUser.username,
+    };
+    const token = this._jwtSrv.sign(payload);
     //Return JWT
-    return;
+    return token;
   }
 
-  async createUser(data: DataSignUp): Promise<User | null | false> {
-    const findUser: User | null | false = await this.findOneByMail(data.email);
-    //TODO: mejorar los return cuando haya un error
-    if (findUser === false) return false; //Error
-    if (!findUser) return null; // Not Found
-
+  async createUser(data: DataSignUp): Promise<User | boolean> {
     const { password } = data;
     //bcrypt psw
-    const pwdToHash: string = await this.bcryptToHash(password);
-    // Save DB
+    const pwdToHash: string = await this._bcryptSrv.bcryptToHash(password);
     data = { ...data, password: pwdToHash };
+    this.logger.log('Creading User...');
+    // Save DB
     const newUser: User = this.userRepository.create(data);
     this.userRepository.save(newUser);
+    this.logger.log(newUser);
     return newUser;
   }
 
   //DB
   async findOneByMail(email: string): Promise<User | null | false> {
     try {
+      this.logger.log('looking for user...');
       const findUser: User | null = await this.userRepository.findOne({
         where: {
           email: email,
@@ -53,17 +65,5 @@ export class AuthService {
       this.logger.error(error);
       return false;
     }
-  }
-
-  //TODO: CREATE SERVICES FOR BCRYPT
-  async bcryptToHash(data: string): Promise<string> {
-    console.log('JII');
-    const x = await hash(data, 10);
-    console.log('JII');
-    return x;
-  }
-
-  async unencrypt(dataHash: string, dataTocompare: string): Promise<boolean> {
-    return await compare(dataTocompare, dataHash);
   }
 }
