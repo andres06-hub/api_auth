c8c55ae8f0ab999008f2b84208f7c03f34e6b009
andres06-hub <andressierrarojas7@gmail.com>
2023-01-23 23:00:54 -0500

    ADD: Methods for Login and SignUp

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { DataLoginDto } from './dto/login-auth.dto';
 import { AuthService } from './auth.service';
+import { DataSignUp } from './dto/signUp.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private _authSrv: AuthService) {}
 
-  @Post()
-  login(@Body() data: DataLoginDto) {
-    this._authSrv.getUser(data);
+  @Post('login')
+  async login(@Body() data: DataLoginDto) {
+    const { email, password } = data;
+    return await this._authSrv.login(email, password);
+  }
+
+  @Post('signup')
+  async signUp(@Body() data: DataSignUp) {
+    return await this._authSrv.createUser(data);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Repository } from 'typeorm';
-import { DataLoginDto } from './dto/login-auth.dto';
 import { User } from 'src/models/user.entity';
+import { hash, compare } from 'bcrypt';
+import { DataSignUp } from './dto/signUp.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,20 +10,60 @@ export class AuthService {
     @Inject('USER_REPOSITORY')
     private userRepository: Repository<User>,
   ) {}
+  private logger = new Logger();
 
-  async getUser(data: DataLoginDto): Promise<User> {
-    // Validate si existe
-    const { email, password } = data;
-    const findUser: User | null = await this.userRepository.findOne({
-      where: {
-        email: email,
-      },
-    });
+  async login(email: string, password: string): Promise<User | null> {
+    const findUser: User | null | false = await this.findOneByMail(email);
     console.log('USER::', findUser);
-    //DB
+    if (!findUser) return null; // NOt found
     //Validate pass
+    const pwd: boolean = await this.unencrypt(findUser.password, password);
+    console.log(pwd);
     //Create JWT
     //Return JWT
     return;
+  }
+
+  async createUser(data: DataSignUp): Promise<User | null | false> {
+    const findUser: User | null | false = await this.findOneByMail(data.email);
+    //TODO: mejorar los return cuando haya un error
+    if (findUser === false) return false; //Error
+    if (!findUser) return null; // Not Found
+
+    const { password } = data;
+    //bcrypt psw
+    const pwdToHash: string = await this.bcryptToHash(password);
+    // Save DB
+    data = { ...data, password: pwdToHash };
+    const newUser: User = this.userRepository.create(data);
+    this.userRepository.save(newUser);
+    return newUser;
+  }
+
+  //DB
+  async findOneByMail(email: string): Promise<User | null | false> {
+    try {
+      const findUser: User | null = await this.userRepository.findOne({
+        where: {
+          email: email,
+        },
+      });
+      return findUser;
+    } catch (error) {
+      this.logger.error(error);
+      return false;
+    }
+  }
+
+  //TODO: CREATE SERVICES FOR BCRYPT
+  async bcryptToHash(data: string): Promise<string> {
+    console.log('JII');
+    const x = await hash(data, 10);
+    console.log('JII');
+    return x;
+  }
+
+  async unencrypt(dataHash: string, dataTocompare: string): Promise<boolean> {
+    return await compare(dataTocompare, dataHash);
   }
 }
