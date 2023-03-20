f7bc07d63b3050b2127bf6e48dbf7d876516ae5a
andres06-hub <andressierrarojas7@gmail.com>
2023-03-20 18:12:45 -0500

    CREATED: interface of JWT payload

---
+++ src/common/interfaces/response.ts
@@ -0,0 +1,10 @@
+export class Response {
+  constructor(success: boolean, message: string, data?: object) {
+    this.success = success;
+    this.message = message;
+    this.data = data;
+  }
+  private success: boolean;
+  private message: string;
+  private data?: object;
+}
