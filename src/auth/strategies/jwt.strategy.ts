4422b40b980442570a36c85bd5f40dd2ff70473d
andres06-hub <andressierrarojas7@gmail.com>
2023-03-20 22:32:12 -0500

    ADD: response

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -6,7 +6,7 @@ import { key } from 'src/config/key.config';
 import { ConfigService } from '@nestjs/config';
 import { AuthService } from '../auth.service';
 import { User } from 'src/models/user.entity';
-import { JwtPayload } from '../interfaces/jwt';
+import { IJwtPayload } from '../interfaces/jwt';
 import { Response } from 'src/common/interfaces/response';
 
 @Injectable()
@@ -23,12 +23,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
   private logger = new Logger();
 
-  async validate(payload: JwtPayload) {
-    this.logger.log('VALIDATION JWT ', payload);
+  async validate(payload: IJwtPayload) {
+    this.logger.log('VALIDATION JWT ');
     const { id } = payload;
     const user: User | null | false = await this._authSrv.findOneById(id);
     if (!user) {
-      throw new UnauthorizedException(new Response(false, 'Unauthorized'));
+      throw new UnauthorizedException(
+        new Response(false, 'Unauthorized', { reason: 'User Not Found!' }),
+      );
     }
     return user;
   }
